7245ff8c79597ed9a792ec6e3a3bdb821301c654
suaaa7 <spuaaa7@gmail.com>
2019-02-06 01:20:22 +0900

    npm install

---
+++ mojipic/conf/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './app/views/index.js',
+  mode: 'none'
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, '../public/javascripts')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { 'modules': false }],
+                '@babel/preset-react'
+              ]
+            }
+          }
+        ],
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  devtool: 'source-map'
+};
+
