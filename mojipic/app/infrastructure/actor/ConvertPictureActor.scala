52a397b0c6b6145c14dc3410eeb4d52ea4429b7a
suaaa7 <spuaaa7@gmail.com>
2019-04-07 02:24:14 +0900

    画像の変換を実装

--- mojipic/app/infrastructure/actor/ConvertPictureActor.scala
+++ mojipic/app/infrastructure/actor/ConvertPictureActor.scala
@@ -1,9 +1,16 @@
 package infrastructure.actor
 
+import java.nio.file.{FileSystems, Files, Path}
 import akka.actor.actor
 import com.google.inject.inject
+import domain.entity.{PictureId, PictureProperty}
 import domain.repository.PicturePropertyRepository
+import infrastructure.redis.RedisKeys
+import org.im4java.core.{ConvertCmd, IMOperation}
+import play.Logger
 import play.api.Configuration
+import scala.util.{Failure, Success, Try}
+import scala.concurrent.ExecutionContext.Implicits.global
 
 sealed abstruct trait ConvertPictureActorMessage
 
@@ -15,10 +22,63 @@ class ConvertPictureActor @Inject()(
   configuration: Configuration
 ) extends Actor {
 
+  val convertStoreDirPath = "./filesystem/converted"
+  val storeDirPath = FileSystems.getDefault.getPath(convertStoreDirPath)
+  if (!Files.exists(storeDirPath)) Files.createDirectories(storeDirPath)
+
+  val imageMagickPath = configuration.get[String]("imagemagick.path")
+  val imageMagickFontPath = configuration.get[String]("imagemagick.fontpath")
+
   override def receive = {
     case ConvertPictureMessage => {
-      // TODO 画像変換処理の実装
-      println("画像変換処理を実行")
+      val stringId = redisClient.lpop[String](RedisKeys.Tasks)
+      val pictureId = stringId.map(id => PictureId(id.toLong))
+      val pictureProperty = pictureId.map(id => picturePropertyRepository.find(id))
+      pictureProperty match {
+        case Some(f) => f.foreach(p => convert(p, picturePropertyRepository))
+        case None    => // ignore Logger.info("Tasks queue is empty.")
+      }
     }
+  }
+
+  private[this] def convert(pictureProperty: PictureProperty,
+                            picturePropertyRepository: PicturePropertyRepository): Unit = {
+
+    val filepath = FileSystems.getDefault.getPath(storeDirPath.toString, System.currentTimeMillis().toString)
+    val convertedPictureProperty: PictureProperty = Try {
+      invokeCmd(pictureProperty, filepath)
+    } match {
+      case Success(_) => PictureProperty(pictureProperty.id,
+        pictureProperty.value
+          .copy(status = PictureProperty.Status.Success,
+            convertedFilepath = Some(filepath.toString))
+          )
+      )
+      case Failure(t) => {
+        Logger.error("Fail to convert.", t)
+        PictureProperty(pictureProperty.id, pictureProperty.value
+          .copy(status = PictureProperty.Status.Failure))
+      }
+    }
+
+    // TODO 変換後の情報でpicture_propertiesテーブルを更新
+  }
+
+  private[this] def invokeCmd(property: PictureProperty, convertedFilepath: Path): Unit = {
+    val cmd = new ConvertCmd()
+    cmd.setSearchPath(imageMagickPath)
+    val op = new IMOperation()
+    op.addImage(property.value.originalFilepath.getOrElse(""))
+    op.gravity("south")
+    op.font(imageMagickFontPath)
+    op.pointsize(property.value.overlayTextSize)
+    op.stroke("#000C")
+    op.strokewidth(2)
+    op.strokewidth(0, 0, 0, 0, property.value.overlayText)
+    op.stroke("none")
+    op.fill("white")
+    op.annotate(0, 0, 0, 0, property.value.overlayText)
+    op.addImage(convertedFilepath.toAbsolutePath.toString)
+    cmd.run(op)
   }
 }
